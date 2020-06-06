6242f40936ecffeeded0c6b4f9f082df75ea546a
Grzegorz Cyran <grzegorz.cyran@interia.pl>
2020-06-06 22:37:52 +0200

    Add some old changes

--- homepage-scripts.js
+++ homepage-scripts.js
@@ -1,4 +1,4 @@
 var time = new Date();
 console.log(`Elo, jest ${time.now}, miło Cię widzieć!`);
 
-alert(`Elo, jest ${time.getFullYear}-${time.getMonth}-${time.getDay}, miło Cię widzieć!`);
+//alert(`Elo, jest ${time.getFullYear}-${time.getMonth}-${time.getDay}, miło Cię widzieć!`);
